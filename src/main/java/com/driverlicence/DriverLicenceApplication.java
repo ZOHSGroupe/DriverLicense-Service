ccee6124834204bee3984796f9b1a2688d429452
SadikHajar <sadik.hajar@etu.uae.ac.ma>
2024-01-25 13:04:59 +0100

    connection  avec eurika et des modification dans docker.compose.yml et dans DriverLicenseApplication

--- src/main/java/com/driverlicence/DriverLicenceApplication.java
+++ src/main/java/com/driverlicence/DriverLicenceApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DriverLicenceApplication {
 
 	public static void main(String[] args) {
